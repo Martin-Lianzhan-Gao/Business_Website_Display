02fcf2f4d30987b30c7ea09ba364261da0b2434e
Martin Gao <martingao@Martindebijibendiannao.local>
2024-09-01 16:25:05 +1000

    fix header layout

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,24 +5,23 @@ import phone from "../assets/phone.png"
 function Header() { 
     return (
         <div className="header">
-            
-            <img src={ logo } alt="Logo" className="logo" />
-            
-            <nav className="header-nav">
-                <ul className={ "header-nav-list" }>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#services">Projects</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </nav>
-            <div id="header-contact">
-                <img src={phone} alt="Phone Logo" className="phone" />
-                <p className="phone-number">0463 666  777</p>
+            <div className="header-content">
+                <img src={ logo } alt="Logo" className="logo" />
+                <nav className="header-nav">
+                    <ul className={ "header-nav-list" }>
+                        <li><a href="#home">HOME</a></li>
+                        <li><a href="#about">ABOUT</a></li>
+                        <li><a href="#services">PROJECTS</a></li>
+                        <li><a href="#contact">CONTACT</a></li>
+                    </ul>
+                </nav>
+                <div id="header-contact">
+                    <img src={phone} alt="Phone Logo" className="phone" />
+                    <p className="phone-number">0463 666  777</p>
+                    <button id="quote-btn">Get A Free Quote</button>
+                </div>
+                
             </div>
-            
-            
-            <button id="quote-btn">Get A Free Quote</button>
         </div>
     );
 }
---
+++ src/Components/Info.jsx
@@ -0,0 +1,19 @@
+
+function Info() {
+    
+    return (
+        <div>
+            <div className="call-us">
+                <img className="phone-logo"></img>
+            </div>
+            <div className="advantage">
+                <img className=""></img>
+            </div>
+            <div className="intro">
+                <p>                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Info;
